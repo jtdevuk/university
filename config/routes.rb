ca5e101cc6c96b455d7fe0e885a98e115b08e583
John Tweeddale <foureggs@hotmail.com>
2020-04-30 16:22:55 +0100

    Complete login and log out functionality

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :courses
   resources :students, except: [:destroy]
+  get 'login', to: 'logins#new'
+  post 'login', to: 'logins#create'
+  delete 'logout', to: 'logins#destroy'
 end
